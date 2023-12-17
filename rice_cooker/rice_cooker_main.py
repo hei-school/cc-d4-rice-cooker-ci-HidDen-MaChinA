41eba6e4c64e0a06c4443cad1507588590bb44d3
HidDen-MaChinA <hei.franco.3@gmail.com>
2023-12-17 14:11:28 +0100

    feat: created the class and the test case

---
+++ rice_cooker/rice_cooker_main.py
@@ -0,0 +1,40 @@
+class rice_cooker:
+    def __init__(self):
+        self.is_plugged_in = False
+        self.is_filled = False
+        self.broken = False
+        self.content = []
+
+    def set_content(self, value=[]):
+        if not isinstance(value, list):
+            raise ValueError('Dude, you need an array')
+        if len(value) > 20:
+            raise ValueError('You can\'t have more than 20 food inside it')
+        self.content = value
+
+    def plug_in_or_out_that_rice_cooker(self):
+        if self.broken:
+            return 'My guy...you threw it into a person...it\'s broken'
+        self.is_plugged_in = not self.is_plugged_in
+        return 'Oke now it\'s plugged in' if self.is_plugged_in else 'Oke now we removed the plug'
+
+    def add_in_some_food_to_cook(self, food):
+        if not self.is_filled:
+            self.content.append(food)
+            if len(self.content) == 20:
+                self.is_filled = True
+            return 'Added a food: ' + food
+        return 'Oke bro...I don\'t know what you are cooking, but I don\'t think it\'s comestible'
+
+    def list_each_food_inside(self):
+        if not self.content:
+            return 'No food inside this'
+        return 'Each food inside the rice-cooker, even if you shouldn\'t have more than rice in it:\n{}'.format('\n'.join(self.content))
+
+    def get_the_dish(self, dish_name):
+        self.content = []
+        return 'Oke, here is your dish: {}\nAnyway, I think all you should do with a rice-cooker is...cook rice. If you wanted to cook some steak or something like that inside of it, the only place you belong is in jail.'.format(dish_name)
+
+    def throw_at_people(self):
+        self.broken = True
+        return 'Oke...you\'re dumb...or you are just...dumb...Oke...anyway the rice-cooker is broken'
---
+++ tests/test_rice_cooker.py
@@ -0,0 +1,30 @@
+import unittest
+from rice_cooker.rice_cooker_main import rice_cooker
+
+class test_rice_cooker(unittest.TestCase):
+    def setUp(self):
+        self.toTest = rice_cooker()
+    def test_plug_in_that_rice_cooker(self):
+        print(self.toTest.is_plugged_in)
+        self.assertEqual(self.toTest.plug_in_or_out_that_rice_cooker(),"Oke now it's plugged in")
+    def test_plug_out_that_rice_cooker(self):
+        self.toTest.plug_in_or_out_that_rice_cooker()
+        self.assertEqual(self.toTest.plug_in_or_out_that_rice_cooker(),"Oke now we removed the plug")
+    def test_add_in_some_food_to_cook(self):
+        self.assertEqual(self.toTest.add_in_some_food_to_cook("foo"),"Added a food: foo")
+    def test_add_in_some_food_fail(self):
+        for i in range(20):
+            self.toTest.add_in_some_food_to_cook(f"food: {i}")
+        self.assertEqual(self.toTest.add_in_some_food_to_cook("last food"),"Oke bro...I don\'t know what you are cooking, but I don\'t think it\'s comestible")
+    def test_list_each_food_inside(self):
+        for i in range(3):
+            self.toTest.add_in_some_food_to_cook(f"food: {i}")
+        self.assertEqual(self.toTest.list_each_food_inside(),"Each food inside the rice-cooker, even if you shouldn\'t have more than rice in it:\nfood: 0\nfood: 1\nfood: 2")
+    def test_list_each_food_if_ther_is_no_food(self):
+        self.assertEqual(self.toTest.throw_at_people(),"Oke...you\'re dumb...or you are just...dumb...Oke...anyway the rice-cooker is broken")
+        self.assertEqual(self.toTest.broken,True)
+    def test_get_the_dish(self):
+        self.assertEqual(self.toTest.get_the_dish("my dish"), "Oke, here is your dish: my dish\nAnyway, I think all you should do with a rice-cooker is...cook rice. If you wanted to cook some steak or something like that inside of it, the only place you belong is in jail.")
+    
+
+unittest.main()
